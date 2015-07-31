55f8329c146291ef3c8d8065019e1337206d9812
Scott Agrimson <scott.agrimson@gmail.com>
2015-07-31 12:14:38 -0700

    add polyfill for *ElementChild, *ElementSibling

--- utils/Polyfills.js
+++ utils/Polyfills.js
@@ -45,6 +45,59 @@ define([], function () {
       };
     }
 
+    (function(undef){
+        if( document.createElement('div').firstElementChild===undef ){
+            Object.defineProperty(Element.prototype, 'firstElementChild', {
+                get : function () { // faster then this.children[0]
+                    var el = this.firstChild;
+                    do {
+                        if(el.nodeType===1){
+                            return el;
+                        }
+                        el = el.nextSibling;
+                    } while(el);
+                    return null;
+                }
+            });
+            Object.defineProperty(Element.prototype, 'lastElementChild', {
+                get : function () {
+                    var el = this.lastChild;
+                    do {
+                        if(el.nodeType===1){
+                            return el;
+                        }
+                        el = el.previousSibling;
+                    } while(el);
+                    return null;
+                }
+            });
+            Object.defineProperty(Element.prototype, 'nextElementSibling', {
+                get : function () {
+                    var el = this.nextSibling;
+                    while(el) {
+                        if(el.nodeType===1){
+                            return el;
+                        }
+                        el = el.nextSibling;
+                    }
+                    return null;
+                }
+            });
+            Object.defineProperty(Element.prototype, 'previousElementSibling', {
+                get : function () {
+                    var el = this.previousSibling;
+                    while(el){
+                        if(el.nodeType===1){
+                            return el;
+                        }
+                        el = el.previousSibling;
+                    }
+                    return null;
+                }
+            });
+        }
+    }(undefined));
+
     (function () {
         Math.sign = Math.sign || function(x) {
           x = +x; // convert to a number
