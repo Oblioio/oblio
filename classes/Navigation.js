524668c2995e0c622ed25c35b72e53acd54a63d7
Scott Agrimson <scott.agrimson@gmail.com>
2016-01-11 12:15:00 -0800

    use oblio.settings.baseUrl if base tag not present

--- classes/Navigation.js
+++ classes/Navigation.js
@@ -34,8 +34,16 @@ define([
         var base = document.getElementsByTagName('base')[0],
             url_arr,
             path_arr;
+
         if (base) {
             url_arr = base.href.split('/');
+            url_arr.pop();
+            oblio.settings.basePath = url_arr.join('/') + '/';
+            path_arr = window.location.href.replace(oblio.settings.basePath, '').split('/');
+            this.currentSection = path_arr[0] !== '' ? path_arr[0] : 'home';
+            this.currentSubsection = path_arr[1];
+        } else if (oblio.settings.baseUrl && oblio.settings.baseUrl !== '') {
+            url_arr = oblio.settings.baseUrl.split('/');
             url_arr.pop();
             oblio.settings.basePath = url_arr.join('/') + '/';
             path_arr = window.location.href.replace(oblio.settings.basePath, '').split('/');
