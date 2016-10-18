87de111d5c4821d7755a784cffedf1afb928dd1c
Jonathan <jonathan@the-flock.net>
2016-10-18 14:45:03 -0700

    add object.assign

--- utils/Polyfills.js
+++ utils/Polyfills.js
@@ -1,5 +1,5 @@
 define([], function () {
-
+    console.log("ADDING POLYFILLS");
     window.getAjax = function (url, success) {
         var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
         xhr.open('GET', url);
@@ -538,5 +538,30 @@ define([], function () {
                 clearTimeout(id);
             };
     }());
+    
+    if (typeof Object.assign != 'function') {
+      (function () {
+        Object.assign = function (target) {
+          'use strict';
+          // We must check against these specific cases.
+          if (target === undefined || target === null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+          }
 
+          var output = Object(target);
+          for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source !== undefined && source !== null) {
+              for (var nextKey in source) {
+                if (source.hasOwnProperty(nextKey)) {
+                  output[nextKey] = source[nextKey];
+                }
+              }
+            }
+          }
+          return output;
+        };
+      })();
+    }
+    
 });
